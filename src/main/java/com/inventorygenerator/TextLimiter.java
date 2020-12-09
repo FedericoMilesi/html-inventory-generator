8e96e93c8c82dcb3f9f596b7278fc5358dd630ea
Federico Milesi <federicomilesi@hotmail.com>
2020-12-09 17:22:40 -0300

    Fixed typo
    wordsLimit (wrong) -> wordlimit (correct)

--- src/main/java/com/inventorygenerator/TextLimiter.java
+++ src/main/java/com/inventorygenerator/TextLimiter.java
@@ -5,17 +5,17 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
 public class TextLimiter extends PlainDocument {
-    private final int wordsLimit;
-    TextLimiter(int wordsLimit) {
+    private final int wordLimit;
+    TextLimiter(int wordLimit) {
         super();
-        this.wordsLimit = wordsLimit;
+        this.wordLimit = wordLimit;
     }
 
     @Override
     public void insertString(int offs, String str, AttributeSet attr) throws BadLocationException {
         if (str == null) return;
 
-        if ((getLength() + str.length()) <= wordsLimit) {
+        if ((getLength() + str.length()) <= wordLimit) {
             super.insertString(offs, str, attr);
         }
     }
